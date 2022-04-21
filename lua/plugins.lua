887a2852850057771aa82b51935c524769afe6ba
Mark Anthony Lumbao <markanthonylumbao@gmail.com>
2022-04-22 03:30:44 +0800

    feat: Utilize treesitter for folding

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,6 +1,6 @@
 vim.cmd([[packadd packer.nvim]])
 
-return require("packer").startup(function()
+return require("packer").startup(function(use)
 	use("wbthomason/packer.nvim")
 	use("L3MON4D3/LuaSnip")
 	use("Yggdroot/indentLine")
--- lua/tree-sitter.lua
+++ lua/tree-sitter.lua
@@ -32,3 +32,34 @@ require("nvim-treesitter.configs").setup({
 		additional_vim_regex_highlighting = false,
 	},
 })
+
+-- Folding setup
+
+local vim = vim
+local api = vim.api
+local M = {}
+
+vim.o.foldmethod = "expr"
+vim.o.foldexpr = "nvim_treesitter#foldexpr()"
+-- function to create a list of commands and convert them to autocommands
+-------- This function is taken from https://github.com/norcalli/nvim_utils
+function M.nvim_create_augroups(definitions)
+	for group_name, definition in pairs(definitions) do
+		api.nvim_command("augroup " .. group_name)
+		api.nvim_command("autocmd!")
+		for _, def in ipairs(definition) do
+			local command = table.concat(vim.tbl_flatten({ "autocmd", def }), " ")
+			api.nvim_command(command)
+		end
+		api.nvim_command("augroup END")
+	end
+end
+
+local autoCommands = {
+	-- other autocommands
+	open_folds = {
+		{ "BufReadPost,FileReadPost", "*", "normal zR" },
+	},
+}
+
+M.nvim_create_augroups(autoCommands)
