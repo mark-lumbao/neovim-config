113cd9c574772e9c35550c85800f1d874363b424
Mark Anthony Ballega Lumbao <markanthonylumbao@gmail.com>
2023-09-18 07:11:19 +0800

    feat: include g:netrw_keepdir config

--- lua/netrw.lua
+++ lua/netrw.lua
@@ -8,8 +8,9 @@ cmd([[
   set updatetime=100
 
   let g:netrw_liststyle=3
-  let g:netrw_banner=0
   let g:netrw_browse_split = 3
+  let g:netrw_banner=0
+  let g:netrw_keepdir = 0
 
   autocmd CursorHold * if (&filetype == 'netrw' && &number == 0) | set nu rnu | endif
 ]])
